72b9bac8722cf0882f6205712028aac5be51ccb2
sos-alena <sev18121976@gmail.com>
2022-05-09 18:12:28 +0300

    Creat task

---
+++ src/com/hilel/MatchOperations.java
@@ -0,0 +1,42 @@
+package com.hilel;
+
+public class MatchOperations {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        int c = a + b;
+
+
+        System.out.println("a + b = " + c);
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        System.out.println("a++ = " + a++);
+        System.out.println(a);
+
+        System.out.println("++a = " + ++a);
+        System.out.println(a);
+
+        System.out.println("a-- = " + a--);
+        System.out.println(a);
+
+        System.out.println("--a = " + --a);
+        System.out.println(a);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/hilel/Task1.java
@@ -0,0 +1,12 @@
+package com.hilel;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+       float a = 10f;
+       float b = 12f;
+
+        System.out.println("Вы прошли за один час " + (a/b) + " тысяч шагов" );
+    }
+}
