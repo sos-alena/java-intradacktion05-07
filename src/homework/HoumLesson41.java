1b53f132a2ee5e9632ddf87e8d3b161ac4e4dd3c
sos-alena <sev18121976@gmail.com>
2022-05-11 14:05:15 +0300

    remove

--- src/homework/HoumLesson41.java
+++ src/homework/HoumLesson41.java
@@ -6,39 +6,35 @@ public class HoumLesson41 {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner pets = new Scanner(System.in);
 
         System.out.println("Input number of dogs: ");
-        int dogs = scanner.nextInt();
+        int dogs = pets.nextInt();
         System.out.println("Input number of cats: ");
-        int cats = scanner.nextInt();
+        int cats = pets.nextInt();
 
         float food = 50;
         int times = 3;
-        float priсe = 2;
+        float price = 2;
 
 
-        int foodDog = foodDog(dogs, food, times);
+        int foodDog = foodPets(dogs, food, times);
         System.out.println("Need food for dogs per month (3 times a day, 50 gm): " + foodDog + "gm;");
 
-        int foodCat = foodCat(cats, food, times);
+        int foodCat = foodPets(cats, food, times);
         System.out.println("Need food for cats per month (3 times a day, 50 gm): " + foodCat + "gm;");
 
-        float T = total(foodDog, foodCat, food, priсe);
+        float T = total(foodDog, foodCat, food, price);
         System.out.println("Need money per month to maintain the shelter (price 2$ for 50 gm): " + T + "$;");
 
     }
 
-    public static int foodDog(int dogs, float food, int times) {
-        return (int) (dogs * food * times);
+    public static int foodPets(int pets, float food, int times) {
+                return (int) (pets * food * times);
     }
 
-    public static int foodCat(int cats, float food, int times) {
-        return (int) (cats * food * times);
-    }
-
-        public static float total(int foodDog,int foodCat, float food, float price){
-            return (float) ((foodDog + foodCat) * price / food);
+    public static float total(int foodDog, int foodCat, float food, float price) {
+        return (float) ((foodDog + foodCat) * price / food);
 
 
     }
