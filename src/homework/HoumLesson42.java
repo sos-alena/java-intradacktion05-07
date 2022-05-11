50ca317ebfa9873572477c200314d1c3ba0822fe
sos-alena <sev18121976@gmail.com>
2022-05-11 15:08:01 +0300

    remove

---
+++ src/homework/HoumLesson42.java
@@ -0,0 +1,25 @@
+package homework;
+
+import java.util.Scanner;
+
+public class HoumLesson42 {
+
+    public static void main(String[] args) {
+
+        Scanner petrolExpense = new Scanner(System.in);
+
+        System.out.println("Input distance, km: ");
+        float distance = petrolExpense.nextInt();
+        System.out.println("Input Expense of petrol total, l: ");
+        float petrol = petrolExpense.nextInt();
+
+        double E = expense(distance, petrol);
+        System.out.println("Expense of petrol 100 km:" + E + "l");
+
+    }
+
+    public static double expense(float distance, float petrol) {
+        return (int) (petrol / distance * 100);
+
+    }
+}
