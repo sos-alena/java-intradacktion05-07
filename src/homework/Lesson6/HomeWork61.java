b195e44cf88c26cae85991b5f7319593686430d9
sos-alena <sev18121976@gmail.com>
2022-05-17 21:19:22 +0300

    remove

--- src/homework/Lesson6/HomeWork61.java
+++ src/homework/Lesson6/HomeWork61.java
@@ -1,8 +1,6 @@
 package homework.Lesson6;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class HomeWork61 {
 
@@ -17,19 +15,21 @@ public class HomeWork61 {
         int number2 = generateRandom();
         System.out.println(number2);
 
+
         int result = minNumber(number, number1, number2);
         System.out.println("Min number " + result);
 
 
     }
 
-    public static int generateRandom() {return (int) (Math.random() * 100);
+    public static int generateRandom() {
+        return (int) (Math.random() * 100);
     }
 
     public static int minNumber(int N, int N2, int N3) {
 
         if (N < N2 && N < N3 || N == N2 && N2 < N3 || N == N3 && N3 < N2) {
-           return (int) (N);
+            return (int) (N);
         } else if (N2 < N && N2 < N3 || N2 == N && N < N3 || N2 == N3 && N3 < N) {
             return (int) (N2);
         } else if (N3 < N && N3 < N2 || N3 == N2 && N2 < N || N3 == N && N < N2) {
