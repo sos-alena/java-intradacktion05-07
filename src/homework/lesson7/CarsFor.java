1e68771a7ec83325c34f3579cd9036be8c8a98de
sos-alena <sev18121976@gmail.com>
2022-05-19 15:44:26 +0300

    remove

--- src/homework/lesson7/CarsFor.java
+++ src/homework/lesson7/CarsFor.java
@@ -4,33 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CarsWhile {
-
+public class CarsFor {
 
     public static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
         String input = "Let's go wash the car!!!";
-        System.out.println(input);
-        int i = 0;
-        while (!input.equalsIgnoreCase("stop")) {
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
 
-            i++;
-            System.out.println();
             stage1();
             stage2();
             stage3();
+
             System.out.println();
             System.out.println("The car has been washed and can be returned to the customer.");
             System.out.println();
             System.out.println("Number of cars washed. №" + i);
-            System.out.println("Enter the word 'go' to continue the program or 'stop' to stop");
-            input = READER.readLine();
-
+            System.out.println();
+            sum++;
         }
-        System.out.println("Total washed cars: " + i);
 
+        System.out.println("Total washed cars: " + sum);
     }
 
     public static void stage1() {
