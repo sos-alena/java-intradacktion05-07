f8d3a21be1d1e38a887ae11f79afeecd38acb797
sos-alena <sev18121976@gmail.com>
2022-07-09 16:29:31 +0300

    refactoring code

--- src/homework/Library/controller/Item.java
+++
@@ -1,7 +0,0 @@
-package homework.library.controller;
-
-public enum Item {
-    AUTHOR,
-    GENRE,
-    BOOK
-}
--- src/homework/Library/controller/Type.java
+++
@@ -1,9 +0,0 @@
-package homework.library.controller;
-
-public enum Type {
-    FIND,
-    CREATE,
-    DELETE,
-    PRINT_ALL,
-    EXIT
-}
